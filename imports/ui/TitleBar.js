530a6fa0e87255ed9ff89c1504ac0a8ea888e4a8
Theo van der Merwe <theo@datatim.co.za>
2019-10-21 13:48:50 +0200

    Initial commit

---
+++ imports/ui/TitleBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default class TitleBar extends React.Component {
+    renderSubtitle() {
+        if (this.props.subTitle) {
+            return (
+                <h2 className="title-bar__subTitle">{this.props.subTitle}</h2>      
+            ) 
+        }
+    }
+    render() {
+        return (
+            <div className="title-bar">
+                <div className="wrapper">
+                    <h1>{this.props.title}</h1>
+                    {this.renderSubtitle()}
+                </div>
+            </div>
+        )
+    }
+};
+
+TitleBar.propTypes = {
+    title: React.PropTypes.string.isRequired,
+    subTitle: React.PropTypes.string
+};
+
+TitleBar.defaultProps = {
+    // title: 'Default Title'
+};
